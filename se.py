10e2be91242fea2b92196dc5efcd5e8bb209356e
vanime <98891705+zizi2525@users.noreply.github.com>
2023-05-06 17:30:45 +0200

    Update se.py

--- se.py
+++ se.py
@@ -335,11 +335,10 @@ def get_player(match_id):
    
    print(source)
    return {"cdn":matches}
-
-
-uvicorn.run(app)
     
     
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=80, reload=True)    
   
 
 
